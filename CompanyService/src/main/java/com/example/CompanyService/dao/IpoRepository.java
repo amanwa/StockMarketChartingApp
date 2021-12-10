abf89e84770cb3e08c6bf7253ed5cd8bca0d5664
amanwa <67272799+amanwa@users.noreply.github.com>
2021-12-10 17:53:08 +0530

    Add files via upload

---
+++ CompanyService/src/main/java/com/example/CompanyService/dao/IpoRepository.java
@@ -0,0 +1,16 @@
+package com.example.CompanyService.dao;
+
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.CompanyService.model.Ipo;
+
+@Repository
+public interface IpoRepository extends MongoRepository<Ipo, String>
+{
+    public Optional<Ipo> findById(String id);
+    public void deleteById(String id);
+}
---
+++ CompanyService/src/main/java/com/example/CompanyService/mapper/StockPriceMapper.java
@@ -0,0 +1,40 @@
+package com.example.CompanyService.mapper;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+import com.example.CompanyService.dto.StockPriceDto;
+import com.example.CompanyService.model.StockPrice;
+
+@Component
+public class StockPriceMapper
+{
+    public StockPriceDto toStockPriceDto(StockPrice stockPrice)
+    {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        StockPriceDto stockPriceDto = mapper.map(stockPrice, StockPriceDto.class);
+        return stockPriceDto;
+    }
+
+    public StockPrice toStockPrice(StockPriceDto stockPriceDto)
+    {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        StockPrice stockPrice = mapper.map(stockPriceDto, StockPrice.class);
+        return stockPrice;
+    }
+
+    public List<StockPriceDto> toStockPriceDtos(List<StockPrice> stockPrices)
+    {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        List<StockPriceDto> stockPriceDtos = Arrays.asList(mapper.map(stockPrices, StockPriceDto[].class));
+        return stockPriceDtos;
+    }
+}
